f3524927a340fa7ea783718ce161998006ec14be
鈦匕工作室 - 貓虎皮 <60348735+MaoHuPi@users.noreply.github.com>
2022-05-04 23:28:26 +0800

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,60 @@
+'use strict';
+let data = {};
+sendXmlhttp('data.json', '', json => {
+    data = JSON.parse(json);
+    for(let titleId in data){
+        let h2 = document.createElement('h2'), 
+            div = document.createElement('div');
+        h2.innerText = data[titleId]['name'];
+        div.id = titleId;
+        div.className = 'checks';
+        for(let buttonId in data[titleId]['check']){
+            let button = document.createElement('div');
+            button.innerText = data[titleId]['check'][buttonId]['name'];
+            button.setAttribute(titleId, buttonId);
+            button.onclick = function(){
+                var newValue = !(this.getAttribute('checked') == 'true');
+                this.setAttribute('checked', newValue);
+                data[titleId]['check'][buttonId]['value'] = newValue;
+                setOutput();
+            };
+            if('attribute' in data[titleId]['check'][buttonId]){
+                for(let attributeId in data[titleId]['check'][buttonId]['attribute']){
+                    let input = document.createElement('input');
+                    input.setAttribute(buttonId, attributeId);
+                    input.type = data[titleId]['check'][buttonId]['attribute'][attributeId]['type'];
+                    input.onchange = function(){
+                        data[titleId]['check'][buttonId]['attribute'][attributeId]['value'] = this.value;
+                    };
+                    button.appendChild(input);
+                }
+            }
+            div.appendChild(button);
+        }
+        $('#data').appendChild(h2);
+        $('#data').appendChild(div);
+    }
+});
+function setOutput(){
+    const newData = {};
+    for(let titleId in data){
+        newData[titleId] = {}
+        for(let buttonId in data[titleId]['check']){
+            if(data[titleId]['check'][buttonId]['value']){
+                newData[titleId][buttonId] = true;
+            }
+            if('attribute' in data[titleId]['check'][buttonId]){
+                newData[titleId][buttonId] = {};
+                for(let attributeId in data[titleId]['check'][buttonId]['attribute']){
+                    newData[titleId][buttonId][attributeId] = data[titleId]['check'][buttonId]['attribute'][attributeId]['value'];
+                }
+            }
+        }
+    }
+    let output = $('#output');
+    output.value = `<script src="${location.protocol}${location.hostname}${location.pathname}script/school1.js"></script>
+<script>
+    let mySchool1 = new school1(${JSON.stringify(newData)});
+    mySchool1.init();
+</script>`;
+}
---
+++ script/school1.js
@@ -0,0 +1,139 @@
+'use strict'
+class school1{
+    constructor(settings = {}){
+        // basic variable
+        this.path = 'https://maohupi.riarock.com/web/tool/318station/file';
+        this.body = document.body;
+        this.$_GET = {};
+        if(location.href.indexOf('?') > -1){
+            location.href.split('?')[1].split('&').forEach(kv => {
+                kv = kv.split('=');
+                this.$_GET[kv[0]] = kv[1];
+            });
+        }
+        var sh = location.href.split('/');
+        this.page = sh[sh.length-1].split('.')[0];
+        this.homeworkMode = 'op' in this.$_GET ? this.$_GET['op'] : 'view';// view | edit_form
+        // copy element
+        this.copyP = this.createElement('p');
+        this.copyA = this.createElement('a');
+        this.copyP.style.margin = '0px';
+        this.copyP.style.padding = '0px';
+        this.copyP.style.width = '0px';
+        this.copyP.style.height = '0px';
+        this.copyP.style.position = 'absolute';
+        this.copyP.style.color = 'transparent';
+        this.copyP.style.opacity = '0';
+        this.copyP.style.pointerEvents = 'none';
+        this.copyP.style.overflow = 'hidden';
+        this.copyP.appendChild(this.copyA);
+        this.body.appendChild(this.copyP);
+        // homework tool
+        this.homeworkTool = {
+            fullScreen: false, 
+            copyContent: false
+        };
+        if('homeworkTool' in settings){
+            for(let key in this.homeworkTool){
+                if(key in settings['homeworkTool']){
+                    this.homeworkTool[key] = settings['homeworkTool'][key];
+                }
+            }
+        }
+        // editor tool
+        this.editorTool = {};
+        if('editorTool' in settings){
+            for(let key in this.editorTool){
+                if(key in settings['editorTool']){
+                    this.editorTool[key] = settings['editorTool'][key];
+                }
+            }
+        }
+        // tool show bool
+        this.show = {
+            homeworkTool: false, 
+            editorTool: false
+        };
+    }
+    // basic function
+    $(e, f = document){return(f.querySelector(e));}
+    $$(e, f = document){return(f.querySelectorAll(e));}
+    vw(){return(window.innerWidth/100);}
+    vh(){return(window.innerHeight/100);}
+    createElement(...e){return(document.createElement(...e));}
+    dictFilter(dict, fun){
+        var newDict = {};
+        for(let key in dict){
+            if(fun(dict[key])){
+                newDict[key] = dict[key];
+            }
+        }
+        return(newDict);
+    }
+    copy(type, text){
+        if(type == 'text'){
+            this.copyA.innerText = text;
+        }
+        var selection = window.getSelection();
+        var selectRange = document.createRange();
+        selectRange.selectNode(this.copyA);
+        selection.removeAllRanges();
+        selection.addRange(selectRange);
+        document.execCommand('copy');
+    }
+    init(){
+        if(this.page == 'homework'){
+            this.show['homeworkTool'] = this.homeworkMode == 'view' ? (Object.keys(this.dictFilter(this.homeworkTool, key => key)).length > 0) : false;
+            this.show['editorTool'] = this.homeworkMode == 'edit_form' ? (Object.keys(this.dictFilter(this.editorTool, key => key)).length > 0) : false;
+        }
+        if(this.show['homeworkTool']){
+            let blockHeaders = $$('.breadcrumb ~ div .row > div > div:nth-child(1)');
+            blockHeaders.forEach(header => {
+                header.style.position = 'relative';
+                header.content = header.parentElement.innerText;
+                // toolBar
+                let toolBar = this.createElement('div');
+                toolBar.className = 's1_toolBar';
+                toolBar.style.height = '100%';
+                toolBar.style.position = 'absolute';
+                toolBar.style.top = '0px';
+                toolBar.style.right = '0px';
+                toolBar.style.display = 'flex';
+                toolBar.style.flexDirection = 'row-reverse';
+                toolBar.style.alignContent = 'center';
+                toolBar.style.justifyContent = 'flex-start';
+                toolBar.style.flexWrap = 'nowrap';
+                toolBar.style.alignItems = 'center';
+                toolBar.style.boxSizing = 'content-box';
+                toolBar.innerButtonStyle = (button) => {
+                    button.style.outline = 'none';
+                    button.style.margin = '10.6px 10.6px 10.6px 0px';
+                    button.style.padding = '0px';
+                    button.style.width = '31.8px';
+                    button.style.height = '31.8px';
+                    button.style.borderRadius = '50%';
+                    button.style.backgroundSize = '100%';
+                    button.style.backgroundColor = 'white';
+                    button.style.backgroundRepeat = 'no-repeat';
+                    button.style.backgroundPosition = 'center';
+                }
+                if(this.homeworkTool['fullScreen']){
+                    let button = this.createElement('button');
+                    toolBar.innerButtonStyle(button);
+                    button.style.backgroundImage = `url(${this.path}/fullscreen.svg)`;
+                    button.style.backgroundSize = '100%';
+                    toolBar.appendChild(button);
+                }
+                if(this.homeworkTool['copyContent']){
+                    let button = this.createElement('button');
+                    toolBar.innerButtonStyle(button);
+                    button.style.backgroundImage = `url(${this.path}/content_copy.svg)`;
+                    button.style.backgroundSize = '75%';
+                    button.onclick = () => {this.copy('text', header.content.split('\n').map((k, i) => `${i}. ${k}`).join('\n'));};
+                    toolBar.appendChild(button);
+                }
+                header.appendChild(toolBar);
+            });
+        }
+    }
+}
