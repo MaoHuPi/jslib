005cf628e202dede872e6bc18bbf78aaef1bb07d
鈦匕工作室 - 貓虎皮 <60348735+MaoHuPi@users.noreply.github.com>
2022-05-16 20:16:35 +0800

    Update main.js

---
+++ cheat/script/main.js
@@ -0,0 +1,13 @@
+$('#output').value = `
+(() => {
+    let xmlhttp = new XMLHttpRequest();
+    let rf = function (){
+        if (xmlhttp.readyState==4) {
+            eval(xmlhttp.responseText+'\nwindow.cheat = new cheat();\nconsole.log(window);');
+        }
+    }
+    xmlhttp.addEventListener("readystatechange", rf);
+    xmlhttp.open("GET", 'https://raw.githubusercontent.com/MaoHuPi/cheat.js/main/cheat.ls');
+    xmlhttp.send();
+})();
+`;
