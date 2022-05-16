5669eee7c57b49bdfb5bd56a34e76da47870c35f
鈦匕工作室 - 貓虎皮 <60348735+MaoHuPi@users.noreply.github.com>
2022-05-16 20:44:53 +0800

    Update main.js

--- cheat/script/main.js
+++ cheat/script/main.js
@@ -1,13 +1,13 @@
-$('#output').value = `(() => {
+$('#output').value = `((variableName) => {
     let xmlhttp = new XMLHttpRequest();
     let rf = function (){
         if (xmlhttp.readyState==4) {
-            eval(xmlhttp.responseText+'window.cheat = new cheat();console.log(window);');
+            eval(xmlhttp.responseText+\`window.\$\{variableName\} = new cheat();console.log(window);\`);
         }
     }
     xmlhttp.addEventListener("readystatechange", rf);
     xmlhttp.open("GET", 'https://maohupi.github.io/jslib/cheat/script/cheat.js');
-    xmlhttp.send();
+    xmlhttp.send('cheat');
 })();`;
 function copyOutput(){
     $('#output').focus();
