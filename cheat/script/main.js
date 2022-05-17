734e63711bc7caeb4f12a07367073cddb9e58654
鈦匕工作室 - 貓虎皮 <60348735+MaoHuPi@users.noreply.github.com>
2022-05-17 09:34:07 +0800

    Update main.js

--- cheat/script/main.js
+++ cheat/script/main.js
@@ -1,14 +1,33 @@
-$('#output').value = `((variableName) => {
+sendXmlhttp('script/cheat.js', '', (script) => {
+    window['cheat.js'] = script;
+}, 'get');
+function setOutput(){
+    var path = location.href.split('/');
+    path[path.length-1] = '';
+    path = path.join('/');
+    $('#output').value = {
+        'html': 
+`
+<script src="${path}script/cheat.js"></script>
+<script>
+    window['cheat'] = new cheat();
+</script>
+`, 
+        'js(xmlhttp)': 
+`((variableName) => {
     let xmlhttp = new XMLHttpRequest();
     let rf = function (){
         if (xmlhttp.readyState==4) {
-            eval(xmlhttp.responseText+\`window.\$\{variableName\} = new cheat();console.log(window);\`);
+            eval(xmlhttp.responseText+\`window.\$\{variableName\} = new cheat();\`);
         }
     }
     xmlhttp.addEventListener("readystatechange", rf);
-    xmlhttp.open("GET", 'https://maohupi.github.io/jslib/cheat/script/cheat.js');
+    xmlhttp.open("GET", '${path}script/cheat.js');
     xmlhttp.send();
-})('cheat');`;
+})('cheat');`, 
+        'js(cheet)': window['cheat.js']
+    }[$('input[name="outputType"]:checked').value];
+}
 function copyOutput(){
     $('#output').focus();
     $('#output').setSelectionRange(0, $('#output').value.length);
