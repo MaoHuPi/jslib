1fd5927c1e698d626a28f997aa51420b94d0d38d
鈦匕工作室 - 貓虎皮 <60348735+MaoHuPi@users.noreply.github.com>
2022-05-17 22:24:22 +0800

    Update home.js

--- shared_/script/home.js
+++ shared_/script/home.js
@@ -8,19 +8,6 @@ sendXmlhttp('shared_/json/home.json', '', json => {
         h2.innerText = data[path]['name'];
         p.innerText = data[path]['illustrate'];
         $('#data').appendChild(h2);
-        $('#data').appendChild(div);
+        $('#data').appendChild(p);
     }
 });
-
-function check(){
-    pointButtons = document.querySelectorAll('[aria-label="領取額外獎勵"]');
-    if(pointButtons.length > 0){
-        pointButtons.forEach(button => {
-            button.click();
-            document.querySelectorAll('h1.tw-title')[0].innerHTML += 'c';
-            console.log('cccccccccc');
-        });
-    }
-    console.log('cs');
-    setTimeout(check, 1e3);
-}
