41e597bf678292616fe7a90a0fa3dc497edeec2c
鈦匕工作室 - 貓虎皮 <60348735+MaoHuPi@users.noreply.github.com>
2022-05-18 15:04:54 +0800

    Update home.js

--- shared_/script/home.js
+++ shared_/script/home.js
@@ -4,10 +4,19 @@ sendXmlhttp('shared_/json/home.json', '', json => {
     data = JSON.parse(json);
     for(let path in data){
         let h2 = document.createElement('h2'), 
-            p = document.createElement('p');
+            p = document.createElement('p'), 
+            div = document.createElement('div');
         h2.innerText = data[path]['name'];
-        p.innerText = data[path]['illustrate'];
+        p.innerHTML = data[path]['illustrate'];
+        div.className = 'buttons';
+        for(let name in data[path]['button']){
+            let button = document.createElement('button')
+            button.innerText = name;
+            button.onclick = () => window.open(data[path]['button'][name]);
+            div.appendChild(button);
+        }
         $('#data').appendChild(h2);
         $('#data').appendChild(p);
+        $('#data').appendChild(div);
     }
 });
